02da8031fe90f291c9c16727ba924562d60761a5
Ricardo Ribalda <ricardo@ribalda.com>
2025-11-14 17:12:24 +0100

    main.py: Update to rejseplanen v2
    Signed-off-by: Ricardo Ribalda <ricardo@ribalda.com>

--- main.py
+++ main.py
@@ -5,102 +5,94 @@ __copyright__ = "Copyright 2017, Ricardo Ribalda"
 __license__ = "GPL"
 
 import requests
-from xml.dom import minidom
 import datetime
 from dateutil import parser
 from dateutil.tz import gettz
+from urllib.parse import quote
+import google.auth
+from google.cloud import secretmanager
 
 from flask import Flask, render_template, request
 
 
-urls = (
-    '/', 'index',
-    '/(station)', 'station',
-)
+def get_secret(secret_name="REJSEPLANEN_API_KEY"):
+
+    _, project_id = google.auth.default()
+
+    client = secretmanager.SecretManagerServiceClient()
+    name = client.secret_version_path(project_id, secret_name, "latest")
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
+
 
 # Values obtained using:
-# https://epsg.io/map#srs=4326&x=12.504158&y=55.667069&z=16 (WGS84)
-# and:
-# http://xmlopen.rejseplanen.dk/bin/rest.exe/stopsNearby?coordX=55673059&coordY=12565557
+# https://www.gps-coordinates.net/
+# www.rejseplanen.dk/api/location.nearbystops?accessId=API_KEY&format=json&lang=en&originCoordLat=55.71531535480527&originCoordLong=12.558932313943863
+# www.rejseplanen.dk/api/departureBoard?accessId=API_KEY&format=json&lang=en&duration=120&id=A%3D1%40O%3DRyparken%20St.%20%28togbus%29%40X%3D12558856%40Y%3D55715403%40U%3D86%40L%3D8650644%40'
 stations = {
     "Home": {
-        "req": "id=1402",
-        "dir": (u"Ny Ellebjerg St.", u"Sjælør St.", u"Mozarts Plads", u"Teglholmen", u"Nørreport St.")
+        "id": "A=1@O=Gartnerivej (Ryparken)@X=12559387@Y=55720553@U=86@L=1402@",
+        "line": "14",
+        "directionFlag": "2",
     },
     "Ryparken": {
-        "req": "id=1404",
-        "dir": (u"Østerbro, Ryparken")
-    },
-    "Work C": {
-        "req": "id=8600701",
-        "dir": (u"Ballerup St.", u"Frederikssund St.", u"Ølstykke St.")
-    },
-    "Work F": {
-        "req": "id=8600742",
-        "dir": (u"Hellerup St.", u"Klampenborg St.")
-    },
-    "Flintholm": {
-        "req": "id=8600736&useBus=0&useMetro=0",
-        "dir": (u"Klampenborg St.")
+        "id": "A=1@O=Ryparken St. (togbus)@X=12558856@Y=55715403@U=86@L=8650644@",
+        "line": "14",
+        "directionFlag": "1",
     },
 }
 
-baseurl = "http://xmlopen.rejseplanen.dk/bin/rest.exe/departureBoard?"
+baseurl = f"https://www.rejseplanen.dk/api/"
 
 
-def parseDeparture(station, dep):
-    if not dep.attributes['direction'].value in station["dir"]:
-        return None
-    if dep.hasAttribute("rtTime"):
-        time = dep.attributes['rtTime'].value
+def parseDeparture(dep):
+    out = dict()
+    out["dir"] = dep["direction"]
+    out["directionFlag"] = dep["directionFlag"]
+    out["name"] = dep["name"]
+
+    # Get time
+    if "rtTime" in dep:
+        time = dep["rtTime"]
     else:
-        time = dep.attributes['time'].value
-    time = dep.attributes['date'].value + " " + time + " KKT"
-    time = parser.parse(time, dayfirst=True, tzinfos={"KKT": gettz("Europe/Copenhagen")})
+        time = dep["time"]
+    time = parser.parse(
+        f"{dep['date']} {time} KKT",
+        dayfirst=True,
+        tzinfos={"KKT": gettz("Europe/Copenhagen")},
+    )
     now = datetime.datetime.now(datetime.timezone.utc)
+    difftime = abs((now - time).total_seconds())
+    out["minutes"] = int(round(abs(difftime) / 60.0))
+    out["lost"] = now > time
+    out["rt"] = "prognosisType" in dep
 
-    if (now > time):
-        difftime = now - time
-    else:
-        difftime = time - now
-    difftime = difftime.seconds
+    return out
 
-    d = dict()
-    d["name"] = dep.attributes['name'].value
-    d["dir"] = dep.attributes['direction'].value
-    d["minutes"] = int(round(abs(difftime) / 60.0))
-    d["lost"] = now > time
-    d["rt"] = dep.hasAttribute("rtTime")
-    return d
 
 def getDepartures(station):
-    response = requests.post(baseurl + station["req"])
-    xmldoc = minidom.parseString(response.content)
-    itemlist = xmldoc.getElementsByTagName('Departure')
-    deps = list()
-    for i in itemlist:
-        d = parseDeparture(station, i)
-        if d is None:
-            continue
-        deps.append(d)
-    return deps
+    url = f"{baseurl}departureBoard?accessId={get_secret()}&format=json&lang=en&duration=120&id={quote(station['id'])}&lines={station['line']}"
+    response = requests.post(url).json()
+
+    departures = tuple(map(parseDeparture, response["Departure"]))
+
+    return [d for d in departures if d["directionFlag"] == station["directionFlag"]]
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template(
-        'index.html',
-        stations=stations.keys(),
-        title="Index")
+    return render_template("index.html", stations=stations.keys(), title="Index")
 
 
-@app.route('/station')
+@app.route("/station")
 def station():
-    station = stations[request.args.get('name')]
+    station = stations[request.args.get("name")]
     deps = getDepartures(station)
-    return render_template('station.html', transport=deps, title="Station")
+    return render_template("station.html", transport=deps, title="Station")
+
 
 if __name__ == "__main__":
-    app.run (host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
